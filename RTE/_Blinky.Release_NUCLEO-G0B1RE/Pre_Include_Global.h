d3aeab1af747b1ca58bacf85a6722ea2446165dd
Christopher Seidl <KeilChris@users.noreply.github.com>
2022-12-13 16:58:28 +0100

    Added task to run the AVH model and NUCLEO-G0B1RE

---
+++ RTE/_Blinky.Release_NUCLEO-G0B1RE/Pre_Include_Global.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky.Release+NUCLEO-G0B1RE' 
+ * Target:  'Blinky.Release+NUCLEO-G0B1RE' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil::Device:STM32Cube HAL:Common:1.4.2 */
+#define USE_HAL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
